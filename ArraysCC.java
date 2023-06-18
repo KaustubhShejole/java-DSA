b18cdd6f4b0d141a1d8031d98663d05a0f898d20
Kaustubh Shivshankar Shejole <sshejole132@gmail.com>
2023-06-19 02:27:39 +0530

    3rd Commit

--- ArraysCC.java
+++ ArraysCC.java
@@ -2,7 +2,17 @@ import java.util.*;
 
 public class ArraysCC {
     public static void main(String args[]) {
-        int marks[] = new int[50];
+        int marks[] = new int[50]; // O stored by default for integer
+        //"" in string array
+        //'' in character array
         int numbers[] = {1, 2, 3};
+
+
+        Scanner sc = new Scanner(System.in);
+        int phy;
+        phy = sc.nextInt();
+
+
+        System.out.println("phy : "+phy);
     }
 }
